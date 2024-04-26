b78761536da011c4b5d03a9eaf2d87849cb86df3
VaishnaviBasukar20 <vaishnavibasutkar20@gmail.com>
2024-04-26 14:11:17 +0530

    first commit

---
+++ components/MyComponents.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+ const MyComponent= () => {
+    return(
+        <div className="text-pink-700 font-semibold">
+            how are you!!!
+        </div>
+    )
+ }
+ export default MyComponent
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MyComponent from '../components/MyComponents.jsx'
+
+export default function App() {
+  return (
+    <div className="text-5xl bg-white-800 font-extrabold">
+      hello everyone!!!!
+
+        i am vaishnavi
+      <MyComponent/>
+
+    </div>
+  )
+}
